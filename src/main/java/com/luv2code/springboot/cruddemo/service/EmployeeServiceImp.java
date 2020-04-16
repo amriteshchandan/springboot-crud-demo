97ed518cfbd48e1108df2265fe8a9cf19841109e
amriteshchandan <amriteshchandan@yahoo.in>
2020-04-16 22:00:11 +0530

    Spring Data JPA - JPARepository

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImp.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImp.java
@@ -1,44 +1,45 @@
 package com.luv2code.springboot.cruddemo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
 @Service
 public class EmployeeServiceImp implements EmployeeService {
 
 	@Autowired
-	@Qualifier(value = "employeeDAOJpaImp")
-	private EmployeeDAO employeeDAO;
+	private EmployeeRepository employeeRepository;
 	
 	@Override
-	@Transactional
 	public List<Employee> getEmployees() {
-		return employeeDAO.getEmployees();
+		return employeeRepository.findAll();
 	}
 
 	@Override
-	@Transactional
 	public Employee getEmployee(int id) {
-		return employeeDAO.getEmployee(id);
+		Optional<Employee> result = employeeRepository.findById(id);
+		Employee employee = null;
+		if (result.isPresent())
+			employee = result.get();
+		else 
+			throw new RuntimeException("No such employee with id " + id);
+		
+		return employee;
 	}
 
 	@Override
-	@Transactional
 	public void saveEmployee(Employee employee) {
-		employeeDAO.saveEmployee(employee);
+		employeeRepository.save(employee);
 	}
 
 	@Override
-	@Transactional
 	public void deleteEmployee(int id) {
-		employeeDAO.deleteEmployee(id);
+		employeeRepository.deleteById(id);
 	}
 
 }
